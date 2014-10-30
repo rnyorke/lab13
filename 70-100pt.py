bf91e6d7dfb025345434840b8c2cb4e8e1c08f2e
rnyorke <rnyorke@gmail.com>
2014-10-30 15:58:40 -0500

    Progress

--- 70-100pt.py
+++ 70-100pt.py
@@ -1,11 +1,8 @@
-#########################################
-#
-#         70-100pt - Making a game
-#
-#########################################
+
+### 70-100pt - Making a game ###
 
 
-# 70pt - Add buttons for left, right and down that move the player circle
+# 70pt - Add buttons for left, right and down that move the player circle - done
 # 100pt - using lab 11 as an example, add in three horizontally scrolling "enemies"
 # Make them scroll at different speeds and directions.
 
@@ -18,8 +15,8 @@ player = drawpad.create_oval(390,580,410,600, fill="blue")
 
 enemy1 = drawpad.create_oval(390,0,410,20, fill = "red")
 enemy1b= drawpad.create_oval(390,0,410,20, fill = "red")
-enemy2 = drawpad.create_oval(390,0,410,20, fill = "red")
-enemy2b= drawpad.create_oval(390,0,410,20, fill = "red")
+enemy2 = drawpad.create_oval(390,300,410,320, fill = "red")
+enemy2b= drawpad.create_oval(390,300,410,320, fill = "red")
 
 direction = 2
 velocity = 0
@@ -57,8 +54,6 @@ class MyApp:
        	    
        	    self.right.bind("<Button-1>", self.rightClicked)
        	    # Bind an event to the first button
-       	    
-       	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=BOTTOM)
 	
@@ -75,7 +70,8 @@ class MyApp:
 	    global enemy8
 	    global enemy9
 	    global enemy10
-	    x1, y1, x2, y2 = drawpad.coords(enemy1)
+        def motion():
+            x1, y1, x2, y2 = drawpad.coords(enemy1)
             x3, y3, x4, y4 = drawpad.coords(enemy1b)
             # Making the enemy wrap around smoothly
             if x2 > 840: 
@@ -86,8 +82,8 @@ class MyApp:
                 direction = 2
                 drawpad.move(circle2,-841, 0 )
                 velocity = 0
-            drawpad.move(circle,direction,0)
-            drawpad.move(circle2,velocity,0)
+            drawpad.move(enemy1,direction,0)
+            drawpad.move(enemy2,velocity,0)
             drawpad.after(1, animate)
 		
 	def upClicked(self, event):   
@@ -106,5 +102,44 @@ class MyApp:
             global oval
             global player
             drawpad.move(player,20,0)
+drawpad.grid(row=0, column=0)
+
+direction = 2
+velocity = 0
+# Creating animation function
+def animate():
+    global direction
+    global velocity
+
+    x1, y1, x2, y2 = drawpad.coords(enemy1)
+    x3, y3, x4, y4 = drawpad.coords(enemy1b)
+    if x2 > 840: 
+        direction = 0
+        drawpad.move(enemy1,-841,0)
+        velocity = 2
+    if x4 > 840:
+        direction = 2
+        drawpad.move(enemy1b,-841, 0 )
+        velocity = 0
+
+
+
+    x5, y5, x6, y6 = drawpad.coords(enemy2)
+    x7, y7, x8, y8 = drawpad.coords(enemy2b)
+    if x6 > 840: 
+        direction = 0
+        drawpad.move(enemy2,-841,0)
+        velocity = 2
+    if x8 > 840:
+        direction = 2
+        drawpad.move(enemy2b,-841, 0 )
+        velocity = 0
+    drawpad.move(enemy1,direction,0)
+    drawpad.move(enemy1b,velocity,0)
+    drawpad.move(enemy2,direction,0)
+    drawpad.move(enemy2b,velocity,0)
+    drawpad.after(1, animate)
+
+animate()
 app = MyApp(root)
 root.mainloop()
